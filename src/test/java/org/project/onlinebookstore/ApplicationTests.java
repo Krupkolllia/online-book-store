33427d6206863beddf5f7a6ac8c7c36a2a32dfdc
Krupkolllia <krupkoilla397@gmail.com>
2026-04-17 00:21:20 +0200

    init commit

---
+++ src/test/java/org/project/onlinebookstore/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
