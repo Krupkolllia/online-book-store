a762b212269f7d9806238a6818457e23fda970cd
Krupkolllia <krupkoilla397@gmail.com>
2026-04-20 22:10:00 +0200

    feat: create BookMapper

--- src/main/java/org/project/onlinebookstore/dto/mapper/BookMapper.java
+++ src/main/java/org/project/onlinebookstore/dto/mapper/BookMapper.java
@@ -1,9 +1,14 @@
-package org.project.onlinebookstore.mapper;
+package org.project.onlinebookstore.dto.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
+import org.project.onlinebookstore.config.MapStructConfig;
+import org.project.onlinebookstore.dto.BookDto;
+import org.project.onlinebookstore.dto.CreateBookRequestDto;
+import org.project.onlinebookstore.model.Book;
 
-@Mapper
-@MapperConfig
-public class BookMapper {
+@Mapper(config = MapStructConfig.class)
+public interface BookMapper {
+    BookDto toDto(Book book);
+
+    Book toModel(CreateBookRequestDto dto);
 }
