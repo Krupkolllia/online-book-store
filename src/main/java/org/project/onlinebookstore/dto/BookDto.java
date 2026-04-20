99dcd6f5f8179f6e78e6c0c9686446a1898175f4
Krupkolllia <krupkoilla397@gmail.com>
2026-04-20 21:37:29 +0200

    chore: add spring boot web starter

---
+++ src/main/java/org/project/onlinebookstore/dto/BookDto.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public record BookDto(Long id,
+                      String title,
+                      String author,
+                      String isbn,
+                      BigDecimal price,
+                      String description,
+                      String coverImage) {}
