4d833316a98305fb42e46ba41653ac8825c1e81c
Krupkolllia <krupkoilla397@gmail.com>
2026-04-29 23:55:40 +0200

    feat(project): document via Swagger

--- src/main/java/org/project/onlinebookstore/controller/BookController.java
+++ src/main/java/org/project/onlinebookstore/controller/BookController.java
@@ -1,5 +1,7 @@
 package org.project.onlinebookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.project.onlinebookstore.dto.book.BookDto;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Books")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -23,27 +26,32 @@ public class BookController {
 
     private final BookService bookService;
 
+    @Operation(summary = "Get all books")
     @GetMapping
     public Page<BookDto> getAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Get a book by id")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
+    @Operation(summary = "Add book to the database")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         return bookService.save(bookRequestDto);
     }
 
+    @Operation(summary = "Update a book (PUT) by id")
     @PutMapping("/{id}")
     public BookDto updateBookById(@PathVariable Long id,
                               @RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         return bookService.update(id, bookRequestDto);
     }
 
+    @Operation(summary = "Delete a book by id")
     @DeleteMapping("/{id}")
     public void deleteBookById(@PathVariable Long id) {
         bookService.deleteById(id);
--- src/main/java/org/project/onlinebookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/org/project/onlinebookstore/dto/book/CreateBookRequestDto.java
@@ -1,5 +1,6 @@
 package org.project.onlinebookstore.dto.book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,15 +8,22 @@ import java.math.BigDecimal;
 import org.project.onlinebookstore.exception.validator.Isbn;
 
 public record CreateBookRequestDto(
+        @Schema(description = "Book title", example = "Clean code")
         @NotBlank
         String title,
+        @Schema(description = "Book author", example = "Robert C. Martin")
         @NotBlank
         String author,
+        @Schema(description = "A unique ISBN number of book", example = "978-0135398548")
         @Isbn
         String isbn,
+        @Schema(description = "Price of the book", example = "100.00")
         @NotNull
         @DecimalMin(value = "0.0")
         BigDecimal price,
+        @Schema(description = "Book's description", example = "Awesome book")
         String description,
+        @Schema(description = "An URL to book's cover image",
+                example = "https://example.com/clean-code.jpg")
         String coverImage
 ) {}
