cdad5416f823755c8cbe1ece498091c33805e786
Krupkolllia <krupkoilla397@gmail.com>
2026-04-30 21:35:51 +0200

    refactor(swagger config): make strings constants

--- src/main/java/org/project/onlinebookstore/config/OpenApiConfig.java
+++ src/main/java/org/project/onlinebookstore/config/OpenApiConfig.java
@@ -7,13 +7,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    private static final String TITLE = "Online Book Store API";
+    private static final String VERSION = "1.0";
+    private static final String DESCRIPTION = "Documentation API";
 
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Online Book Store API")
-                        .version("1.0")
-                        .description("Documentation API"));
+                        .title(TITLE)
+                        .version(VERSION)
+                        .description(DESCRIPTION));
     }
 }
