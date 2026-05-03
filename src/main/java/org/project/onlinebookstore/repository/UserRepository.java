1b33e203f8d5c32e469f97dcd25a218d2b879d4a
Krupkolllia <krupkoilla397@gmail.com>
2026-05-03 18:04:31 +0200

    feat(security): implement custom UserDetailsService

--- src/main/java/org/project/onlinebookstore/repository/UserRepository.java
+++ src/main/java/org/project/onlinebookstore/repository/UserRepository.java
@@ -3,6 +3,10 @@ package org.project.onlinebookstore.repository;
 import org.project.onlinebookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
