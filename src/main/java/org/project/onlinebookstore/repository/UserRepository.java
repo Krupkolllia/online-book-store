696c72ffce879b7d235ffac1e82e21dc4da351c5
Krupkolllia <krupkoilla397@gmail.com>
2026-05-03 20:28:36 +0200

    feat(security): add security config class

--- src/main/java/org/project/onlinebookstore/repository/UserRepository.java
+++ src/main/java/org/project/onlinebookstore/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package org.project.onlinebookstore.repository;
 
+import java.util.Optional;
 import org.project.onlinebookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
