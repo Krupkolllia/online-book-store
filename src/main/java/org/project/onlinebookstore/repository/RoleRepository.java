3eac5c4eea6cff17d041622b1006da9fe0390dad
Krupkolllia <krupkoilla397@gmail.com>
2026-05-03 21:32:22 +0200

    feat(registration): make register method assign default role

--- src/main/java/org/project/onlinebookstore/repository/RoleRepository.java
+++ src/main/java/org/project/onlinebookstore/repository/RoleRepository.java
@@ -5,6 +5,8 @@ import org.project.onlinebookstore.model.Role;
 import org.project.onlinebookstore.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName roleName);
 }
