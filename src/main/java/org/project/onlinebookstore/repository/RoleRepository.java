bac7437f04fe0fcfd8ab355554917a2b256b1e53
Krupkolllia <krupkoilla397@gmail.com>
2026-05-03 20:55:31 +0200

    feat(role): create role repository

---
+++ src/main/java/org/project/onlinebookstore/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package org.project.onlinebookstore.repository;
+
+import org.project.onlinebookstore.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
