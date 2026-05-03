5a00e408d1ec4e337e7a31c9f802f9a0fcb82de7
Krupkolllia <krupkoilla397@gmail.com>
2026-05-03 21:32:22 +0200

    feat(registration): make register method assign default role

--- src/main/java/org/project/onlinebookstore/repository/RoleRepository.java
+++ src/main/java/org/project/onlinebookstore/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package org.project.onlinebookstore.repository;
 
 import org.project.onlinebookstore.model.Role;
+import org.project.onlinebookstore.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
--- src/main/java/org/project/onlinebookstore/service/impl/UserServiceImpl.java
+++ src/main/java/org/project/onlinebookstore/service/impl/UserServiceImpl.java
@@ -5,16 +5,24 @@ import org.project.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import org.project.onlinebookstore.dto.user.UserResponseDto;
 import org.project.onlinebookstore.exception.RegistrationException;
 import org.project.onlinebookstore.mapper.UserMapper;
+import org.project.onlinebookstore.model.Role;
+import org.project.onlinebookstore.model.RoleName;
 import org.project.onlinebookstore.model.User;
+import org.project.onlinebookstore.repository.RoleRepository;
 import org.project.onlinebookstore.repository.UserRepository;
 import org.project.onlinebookstore.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
 
     private final UserMapper userMapper;
 
@@ -26,7 +34,15 @@ public class UserServiceImpl implements UserService {
         }
 
         User user = userMapper.toModel(requestDto);
-        userRepository.save(user);
-        return userMapper.toDto(user);
+        assignDefaultRole(user);
+        User savedUser = userRepository.save(user);
+        return userMapper.toDto(savedUser);
+    }
+
+    private void assignDefaultRole(User user) {
+        Role role = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("ROLE_USER not found"));
+
+        user.setRoles(new HashSet<>(Set.of(role)));
     }
 }
