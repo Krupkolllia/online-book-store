b77486ab4eb96825aeadcca628d152e8ca45506d
Krupkolllia <krupkoilla397@gmail.com>
2026-05-06 21:40:38 +0200

    feat(login): create needed for login dtos

---
+++ src/main/java/org/project/onlinebookstore/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,12 @@
+package org.project.onlinebookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String password
+) {}
