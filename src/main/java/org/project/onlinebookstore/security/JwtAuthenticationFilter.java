45be053139a8a0e31f4b40a7629a2442ab986ae9
Krupkolllia <krupkoilla397@gmail.com>
2026-05-07 03:12:02 +0200

    feat(login): add /login endpoint to the controller

--- src/main/java/org/project/onlinebookstore/security/JwtAuthenticationFilter.java
+++ src/main/java/org/project/onlinebookstore/security/JwtAuthenticationFilter.java
@@ -4,23 +4,20 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String SCHEMA_PREFIX = "Bearer ";
-
-    private static final String AUTHORIZATION_HEADER = "Authorization";
 
     private final JwtUtil jwtUtil;
 
@@ -30,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String raw = request.getHeader(AUTHORIZATION_HEADER);
+        String raw = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (raw == null || !raw.startsWith(SCHEMA_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
--- src/main/java/org/project/onlinebookstore/security/JwtUtil.java
+++ src/main/java/org/project/onlinebookstore/security/JwtUtil.java
@@ -3,9 +3,9 @@ package org.project.onlinebookstore.security;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import java.util.Date;
 
 @Component
 public class JwtUtil {
