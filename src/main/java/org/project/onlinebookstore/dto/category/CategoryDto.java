cb7e2905320a8e407349c67dae79a65e14e85c2e
Krupkolllia <krupkoilla397@gmail.com>
2026-05-10 17:50:17 +0200

    feat(category): create dto

---
+++ src/main/java/org/project/onlinebookstore/dto/category/CategoryDto.java
@@ -0,0 +1,10 @@
+package org.project.onlinebookstore.dto.category;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CategoryDto(
+        Long id,
+        @NotBlank
+        String name,
+        String description
+) {}
