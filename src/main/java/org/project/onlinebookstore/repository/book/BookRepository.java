3d3747e5199f5041a03a2beb6207ba0c14108669
Krupkolllia <krupkoilla397@gmail.com>
2026-05-12 00:15:07 +0200

    fix(book): add entity graph annotation to book repository find methods

--- src/main/java/org/project/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/org/project/onlinebookstore/repository/book/BookRepository.java
@@ -1,9 +1,22 @@
 package org.project.onlinebookstore.repository.book;
 
+import java.util.Optional;
 import org.project.onlinebookstore.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(@NonNull Pageable pageable);
 
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = "categories")
+    Optional<Book> findById(@NonNull Long id);
 }
