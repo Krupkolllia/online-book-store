cb3f7f3cd7267b08aa21b47ca98d782d35a1b804
Krupkolllia <krupkoilla397@gmail.com>
2026-05-13 01:24:21 +0200

    feat(cart item): create cart item repository

---
+++ src/main/java/org/project/onlinebookstore/repository/cart/CartItemRepository.java
@@ -0,0 +1,11 @@
+package org.project.onlinebookstore.repository.cart;
+
+import java.util.Optional;
+import org.project.onlinebookstore.model.CartItem;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @EntityGraph(attributePaths = {"book"})
+    Optional<CartItem> findByIdAndShoppingCartUserId(Long id, Long userId);
+}
