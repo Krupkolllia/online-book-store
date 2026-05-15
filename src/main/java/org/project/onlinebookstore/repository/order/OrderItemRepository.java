940f844d7fcad26ae53e8d35b933d85c119287d4
Krupkolllia <krupkoilla397@gmail.com>
2026-05-16 00:07:59 +0200

    feat(order item): create order item repository

---
+++ src/main/java/org/project/onlinebookstore/repository/order/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package org.project.onlinebookstore.repository.order;
+
+import java.util.Optional;
+import org.project.onlinebookstore.model.order.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Page<OrderItem> findAllByOrderIdAndOrderUserId(Long orderId, Long userId, Pageable pageable);
+
+    Optional<OrderItem> findByIdAndOrderIdAndOrderUserId(Long id, Long orderId, Long userId);
+}
