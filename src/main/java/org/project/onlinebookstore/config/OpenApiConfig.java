aa8543ceea022aec5dfc5c373fe11db2f12008b4
Krupkolllia <krupkoilla397@gmail.com>
2026-05-16 14:45:22 +0200

    chore(security): add jwt auth to swagger

--- src/main/java/org/project/onlinebookstore/config/OpenApiConfig.java
+++ src/main/java/org/project/onlinebookstore/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package org.project.onlinebookstore.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +20,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title(TITLE)
                         .version(VERSION)
-                        .description(DESCRIPTION));
+                        .description(DESCRIPTION))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
