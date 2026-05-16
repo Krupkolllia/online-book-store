ce3864d82ad54225258ea94a6aeb3d6afb0848a9
Krupkolllia <krupkoilla397@gmail.com>
2026-05-16 13:48:13 +0200

    feat(order item): implement order item service

--- src/main/java/org/project/onlinebookstore/service/OrderItemService.java
+++ src/main/java/org/project/onlinebookstore/service/OrderItemService.java
@@ -1,4 +1,11 @@
 package org.project.onlinebookstore.service;
 
+import org.project.onlinebookstore.dto.order.OrderItemResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface OrderItemService {
+    Page<OrderItemResponseDto> findItemsByOrderId(Long orderId, Pageable pageable);
+
+    OrderItemResponseDto findItemByIdInOrder(Long orderId, Long itemId);
 }
